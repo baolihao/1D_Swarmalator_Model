1bba3adfaedb618ef8243924042bb6071ffd519f
B.H <58239833+baolihao@users.noreply.github.com>
2023-03-08 14:32:35 -0600

    Delete energy_phi.m

--- energy_phi.m
+++
@@ -1,9 +0,0 @@
-function phi = energy_phi(r, z, A, J, B)
-% function phi = energy_phi(r, z, A, J, B)
-
-% M. Zhong
-
-phi      = zeros(size(r));
-ind      = r > 0;
-phi(ind) = (A + J * cos(z(ind)))./r(ind) - B./r(ind).^2;
-end
