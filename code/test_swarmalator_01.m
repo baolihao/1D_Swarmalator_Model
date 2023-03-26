e4e83c0784de2d681f84ffda03b33ec1876ab847
Ming Zhong <mingjzhong@gmail.com>
2023-03-26 16:38:44 -0500

    added two things
    1) the alignment based phi
    2) finished the example on running Swarmalator-cucker-smale

--- code/test_swarmalator_01.m
+++ code/test_swarmalator_01.m
@@ -8,13 +8,13 @@ N                       = 100;          % Number of agents
 d                       = 2;             % dimension of the physical space
 ode_type                = 0;             % type of the ODE system
                                          % 0 - original, cluster and sync
-                                         % 1 - original + Viscek
+                                         % 1 - original + Vicsek
                                          % 2 - original + Cucker-Smale
+                                         % 3 - normal Cucker-Smale
+                                         % 4 - normal Vicsek
 r_neighbor              = 1;             % the size of the neighborhood
 % initial distribution for x
 x0                      = 2 * rand(d, N);
-% initial distribution for xi
-xi0                     = 2 * pi * rand(1, N) - pi;
 % set up the parameters
 A                       = 1;
 B                       = 1;
@@ -24,14 +24,30 @@ K                       = 1;
 switch ode_type
   case 0
     phi                 = {@(r, z) energy_phi(r, z, A, J, B), @(r, z) xi_phi(r, z, K)};
+% initial distribution for xi
+    xi0                 = 2 * pi * rand(1, N) - pi;    
     y0                  = [x0(:); xi0(:)];
   case 1
-    theta0              = 2 * pi * rand(1, N) - pi;
     phi                 = {@(r, s, z) energy_phi(r, z, A, J, B), @(r, s, z) ones(size(s)), ...
                            @(r, s, z) xi_phi(r, z, K)};
+    theta0              = 2 * pi * rand(1, N) - pi;
+% initial distribution for xi
+    xi0                 = 2 * pi * rand(1, N) - pi;      
     y0                  = [x0(:); theta0(:); xi0(:)];
   case 2
+    % we take K = sigma = 1, and beta = 0.25;
+    K                   = 1;
+    sigma               = 1;
+    beta                = 0.25;
+    phi                 = {@(r, s, z) energy_phi(r, z, A, J, B), @(r, s, z) alignment_phi(r, K, ...
+                           sigma, beta), @(r, s, z) xi_phi(r, z, K)};    
+% initial distribution for xi
+    xi0                 = 2 * pi * rand(1, N) - pi;      
     v0                  = rand(d, N);
+    y0                  = [xo(:); v0(:); xi0(:)];
+  otherwise
+    % need more phis on 3 and 4
+    error('');
 end
 % time interval for intergration
 tspan                   = [0, T];
