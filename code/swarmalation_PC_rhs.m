e8da4ff90e881e2658f0180037347bb07ea1c9b4
Ming Zhong <mingjzhong@gmail.com>
2023-04-28 14:07:15 -0500

    re-named some files for the proper swarmalation on ring

--- code/swarmalation_PC_rhs.m
+++ code/swarmalation_PC_rhs.m
@@ -1,5 +1,6 @@
-function rhs = swarmalation_on_ring_rhs(y, J, K, sigma, N)
-% function rhs = swarmalation_on_ring_rhs(y, J, K, sigma, N)
+function rhs = swarmalation_PC_rhs(y, J, K, sigma, N)
+% function rhs = swarmalation_PC_rhs(y, J, K, sigma, N)
+% swarmalator on Polar Coordinates
 
 % (C) M. Zhong
 
@@ -25,13 +26,13 @@ Z_0       = 1/N * sum(exp(1i * thetaj));
 R_0       = abs(Z_0);
 Psi_0     = angle(Z_0);
 % not sure if it should be the 1st momement or 2nd moment
-Z_1       = 1/N * sum(rj .* exp(1i * thetaj));
-R_1       = abs(Z_1);
-Psi_1     = angle(Z_1);
+% Z_1       = 1/N * sum(rj .* exp(1i * thetaj));
+% R_1       = abs(Z_1);
+% Psi_1     = angle(Z_1);
 % 2nd moment
-% Z_2      = 1/N * sum(r.^(2) .* exp(1i * theta));
-% R_2      = abs(Z_2);
-% Psi_2    = angle(Z_2);
+Z_2      = 1/N * sum(rj.^(2) .* exp(1i * thetaj));
+R_2      = abs(Z_2);
+Psi_2    = angle(Z_2);
 %
 phiptheta = phij + thetaj;
 phimtheta = phij - thetaj;
@@ -51,7 +52,7 @@ r_dot     = H_r - J * rj * R_0 .* cos(diff3) + J/2 * (S_tilde_p * cos(diff1) + S
 H_phi     = 1/N * sum((rj ./ ri) .* sin(phiij) .* mod, 2);
 phi_dot   = H_phi + J./(2 * rj) .* (S_tilde_p * sin(diff1) + S_tilde_m * sin(diff2));
 % (R_1, Psi_1) might be (R_2, Psi_2)
-theta_dot = K * (1 - rj.^(2)/sigma^2) * R_0 .* sin(diff3) - K/sigma^2 * R_1 * sin(Psi_1 - thetaj) ...
+theta_dot = K * (1 - rj.^(2)/sigma^2) * R_0 .* sin(diff3) - K/sigma^2 * R_2 * sin(Psi_2 - thetaj) ...
             + K * rj/sigma^2 .* (S_tilde_p * sin(diff1) + S_tilde_m * sin(diff2));
 rhs       = [r_dot; phi_dot; theta_dot];
 end
