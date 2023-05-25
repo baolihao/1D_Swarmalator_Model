14f34531d4808f310e4f45e7c97949f309653cc3
Baoli Hao <58239833+baolihao@users.noreply.github.com>
2023-05-24 23:34:32 -0500

    Update swarmalation_1D_ring_rhs.m

--- code/swarmalation_1D_ring_rhs.m
+++ code/swarmalation_1D_ring_rhs.m
@@ -1,10 +1,10 @@
 function rhs= swarmalation_1D_ring_rhs(y,J,K,N)
 
-    phij        = y(1:N);
-    phii        = phij';
+    phii        = y(1:N);
+    phij        = phii';
 
-    thetaj      = y(N+1:2*N);
-    thetai      = thetaj';
+    thetai      = y(N+1:2*N);
+    thetaj      = thetai';
 
     phi_dot     = J/N.*sum(sin(phij-phii).*cos(thetaj-thetai),2);
     theta_dot   = K/N.*sum(sin(thetaj-thetai).*cos(phij-phii),2);
