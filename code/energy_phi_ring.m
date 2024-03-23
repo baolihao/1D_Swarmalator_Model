8a982ea61185912a6898464d1205d1cbb60e69fd
Baoli Hao <58239833+baolihao@users.noreply.github.com>
2024-03-22 23:46:07 -0500

    Delete code directory

--- code/energy_phi_ring.m
+++
@@ -1,9 +0,0 @@
-function phi = energy_phi_ring(r, z, J)
-% function phi = energy_phi_ring(r, z, J)
- 
-% (C) M. Zhong
-
-phi      = zeros(size(r));
-ind      = r > 0;
-phi(ind) = 1 + J * cos(z(ind)) - r(ind).^(-2);
-end
